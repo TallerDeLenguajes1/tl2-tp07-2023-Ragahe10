34474eae1ceb795a76078318fbd8c73f80c6cf08
Ragahe10 <ragahe10@gmail.com>
2023-10-06 18:30:04 -0300

    Clase Tarea

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,1 +1,17 @@
 namespace EspacioTarea;
+public enum EstadoTarea{
+    pendiente,
+    enProgreso,
+    Completada
+}
+public class Tarea{
+    private int id;
+    private string titulo;
+    private string descripcion;
+    private EstadoTarea estadoTarea;
+
+    public int Id { get => id; set => id = value; }
+    public string Titulo { get => titulo; set => titulo = value; }
+    public string Descripcion { get => descripcion; set => descripcion = value; }
+    public EstadoTarea EstadoTarea { get => estadoTarea; set => estadoTarea = value; }
+}
